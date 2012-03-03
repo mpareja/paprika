6b0a37c081782f7fdd78a107917f41e8e8305c3f
Mario Pareja <pareja.mario@gmail.com>
2012-03-03 02:47:11 -0500

    Use the zip executable in the tools directory.

--- spec/zip.spec.js
+++ spec/zip.spec.js
@@ -5,6 +5,20 @@ describe('zip', function () {
   beforeEach(function() {
     // reset arch
     process.arch = arch;
+
+    this.addMatchers({
+      toEndWith: function(expected) {
+        var ix = this.actual.lastIndexOf(expected)
+          , length = this.actual.length;
+
+        if (ix < 0)
+          return false;
+        return (length - ix) === expected.length;
+      },
+      toStartWith: function(expected) {
+        return this.actual.indexOf(expected) === 0;
+      }
+    });
   });
 
   describe('on windows', function () {
@@ -24,13 +38,19 @@ describe('zip', function () {
     it('should use the 32-bit executable on 32-bit machines', function() {
       process.arch = 'ia32';
       go();
-      expect(procname).toEqual('zip-x86.exe');
+      expect(procname).toEndWith('zip-x86.exe');
     });
 
     it('should use the 64-bit executable on 64-bit macines', function () {
       process.arch = 'x64';
       go();
-      expect(procname).toEqual('zip-x64.exe');
+      expect(procname).toEndWith('zip-x64.exe');
+    });
+
+    it('should use the zip executable in the tools dir.', function () {
+      go();
+      expect(procname).toStartWith(
+        require('path').join(process.cwd(), 'tools'));
     });
   });
 });
