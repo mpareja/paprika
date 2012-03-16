ad5d917386c4edfa078b32bccb2563f355d51bf2
Mario Pareja <pareja.mario@gmail.com>
2012-03-16 09:17:05 -0400

    aspcompile: Added integration test. Failing early if run on a non-Windows machine.

--- spec/aspcompile.spec.js
+++ spec/aspcompile.spec.js
@@ -1,35 +1,74 @@
 var aspcompile = require('../lib/paprika').aspcompile;
 
 describe('aspcompile', function () {
-  it('should require the virtual path', function () {
-    expect(function () {
-      aspcompile({ physicalPath: '/some/path' });
-    }).toThrow(new Error('Missing parameter: virtualPath.'));
+  describe('parameters', function () {
+    it('should require the virtual path', function () {
+      expect(function () {
+        aspcompile({ physicalPath: '/some/path' });
+      }).toThrow('Missing parameter: virtualPath.');
+    });
+
+    it('should require the physical path', function () {
+      expect(function () {
+        aspcompile({ virtualPath: '/myapp' });
+      }).toThrow('Missing parameter: physicalPath.');
+    });
+
+    it('should require the .NET Framework version', function () {
+      expect(function () {
+        aspcompile({
+          virtualPath: '/myapp',
+          physicalPath: '/some/path',
+          processor: 'x86'
+        });
+      }).toThrow('Missing parameter: version.');
+    });
+
+    it('should require the .NET Framework processor architecture to use.', function () {
+      expect(function () {
+        aspcompile({
+          virtualPath: '/myapp',
+          physicalPath: '/some/path',
+          version: 'net35'
+        });
+      }).toThrow('Missing parameter: processor.');
+    });
   });
 
-  it('should require the physical path', function () {
-    expect(function () {
-      aspcompile({ virtualPath: '/myapp' });
-    }).toThrow(new Error('Missing parameter: physicalPath.'));
+  describe('non-windows OS', function () {
+    var platform = process.platform;
+    beforeEach(function () { process.platform = 'linux'; });
+    afterEach(function () { process.platform = platform; });
+
+    it('should not be able to perform compilation outside of Windows', function () {
+      expect(function () {
+        aspcompile({
+          physicalPath: './does/not/exist',
+          virtualPath: './also/does/not/exist',
+          processor: 'x86',
+          version: 'net35'
+        });
+      }).toThrow('FAILED: ASP.NET Compilation is only supported on Windows.');
+    });
   });
 
-  it('should require the .NET Framework version', function () {
-    expect(function () {
+  describe('integration tests', function () {
+    if (process.platform !== 'win32') {
+      return; // don't perform these tests on linux
+    }
+
+    it('should receive callback with error code on failure', function () {
+      var callback = function (code) {
+        expect(code).toEqual(1);
+        asyncSpecDone();
+      }
       aspcompile({
-        virtualPath: '/myapp',
-        physicalPath: '/some/path',
-        processor: 'x86'
-      });
-    }).toThrow(new Error('Missing parameter: version.'));
-  });
-
-  it('should require the .NET Framework processor architecture to use.', function () {
-    expect(function () {
-      aspcompile({
-        virtualPath: '/myapp',
-        physicalPath: '/some/path',
-        version: 'net35'
-      });
-    }).toThrow(new Error('Missing parameter: processor.'));
+          physicalPath: './does/not/exist',
+          virtualPath: './also/does/not/exist',
+          processor: 'x86',
+          version: 'net35'
+        }, callback);
+      asyncSpecWait();
+    });
   });
 });
