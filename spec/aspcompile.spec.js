95e82fb7aca48e064307fdb257ef644f7cccf7d5
Mario Pareja <pareja.mario@gmail.com>
2012-03-16 18:05:31 -0400

    aspcompile: Support specifying default parameters.

--- spec/aspcompile.spec.js
+++ spec/aspcompile.spec.js
@@ -35,6 +35,58 @@ describe('aspcompile', function () {
     });
   });
 
+  describe('configuring default parameters', function () {
+    var run = aspcompile.run,
+      cmd = null,
+      args = null,
+      mockRun = function (foundcmd, foundargs) {
+        cmd = foundcmd;
+        args = foundargs;
+      };
+
+    beforeEach(function () { aspcompile.run = mockRun; });
+    afterEach(function () {
+      aspcompile.run = run;
+      cmd = null;
+      args = null;
+    });
+
+    function setDefaultAndAssert(option, value) {
+      var defaults = {};
+      defaults[option] = value;
+      aspcompile.setDefaults(defaults);
+
+      var options = {
+          virtualPath: '/myapp',
+          physicalPath: '/some/path',
+          processor: 'x86',
+          version: 'net40'
+      };
+      delete options[option];
+      aspcompile(options); // will throw if option not defined
+    }
+
+    it('should allow specifying the default version', function () {
+      setDefaultAndAssert('version', 'net35');
+      expect(cmd).toMatch(/v2\.0/);
+    });
+
+    it('should allow specifying the default processor', function () {
+      setDefaultAndAssert('processor', 'x64');
+      expect(cmd).toMatch(/Framework64/);
+    });
+
+    it('should allow specifying the default virtualPath', function () {
+      setDefaultAndAssert('virtualPath', '/some/other/path');
+      expect(args).toContain('/some/other/path');
+    });
+
+    it('should allow specifying the default physicalPath', function () {
+      setDefaultAndAssert('physicalPath', '/a/different/path');
+      expect(args).toContain('/a/different/path');
+    });
+  });
+
   describe('non-windows OS', function () {
     var platform = process.platform;
     beforeEach(function () { process.platform = 'linux'; });
